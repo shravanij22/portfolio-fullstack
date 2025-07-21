c7cce01f91aa1756ca4b28243e008db70222da8c
shravanij22 <147096064+shravanij22@users.noreply.github.com>
2025-07-21 23:20:06 +0530

    Services C0mplete

---
+++ app/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='w-full px-[12%] py-10 scroll-mt-20'>
+      <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
+      <h2 className='text-center text-5xl font-Ovo'>My Latest Work</h2>
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+        I have worked on a variety of projects that showcase my skills in web development. 
+        From building responsive websites to developing complex web applications, I take pride in delivering high-quality work that meets the needs of my clients.
+        Below are some of the projects I have completed recently. Each project reflects my commitment to excellence.
+      </p>
+    </div>
+  )
+}
+
+export default Contact
